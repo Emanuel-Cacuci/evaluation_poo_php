5c5eca1f88db9406e165fbc275feaaa92f8da851
emanuelcacucipro@gmail.comgit config --global user.email emanuelcacucipro@gmail.com <emanuelcacucipro@gmail.com>
2025-04-29 14:15:13 +0200

    eval_poo

---
+++ evaluation_php/class.php
@@ -0,0 +1,60 @@
+<h1>Créez le Model destiné à un site de vente de vêtements. 
+Les vêtements seront définis par un id, un nom, une catégorie et un prix.  
+Typez toutes les propriétés et méthodes de la classe. </h1>
+
+
+<?php
+
+// Class parent (Model)
+
+abstract class Model{
+ 
+    protected int $id;
+    protected string $nom;
+    protected string $categorie;
+    protected float $prix;
+
+    public function __construct(int $id,string $nom, string $categorie, float $prix)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->categorie = $categorie;
+        $this->prix = $prix;
+
+    }
+
+}
+
+// Class enfant qui hérite de la classe parent (Model)
+
+class Vetments extends Model{
+
+public function afficherProduits(): void
+{
+    
+        echo "L'id : $this->id <br> Nom : $this->nom <br> Categorie : $this->categorie <br> Prix : $this->prix €";
+        
+}
+
+}
+
+// Class enfant qui hérite de la classe parent (Model)
+
+class Voiture extends Model{
+
+    
+    public function afficherProduits(): void
+    {
+        
+         echo "L'id : $this->id <br> Nom : $this->nom <br> Categorie : $this->categorie <br> Prix : $this->prix €";
+        
+    }
+}
+
+$vetments=new Vetments(0,"Chemise", "Vetment", 20);
+$vetments->afficherProduits();
+echo "<br><br>";
+$voiture=new Voiture(1,"Golf", "Voiture", 5600);
+$voiture->afficherProduits();
+
+?>
---
+++ evaluation_php/questions.php
@@ -0,0 +1,51 @@
+<h1>I. Qu'est-ce qu'une classe en PHP ?</h1>
+
+<p>Une class permet de creer des objets, il y a des propriétés, des paramétrés et des méthodes</p>
+
+
+<h1>II. Qu'est-ce qu'un objet en PHP ? </h1>
+
+<p>Les valeurs des proprietes ?</p>
+
+
+<h1>III. Comment crée-t-on un objet ? </h1>
+
+<p>$exemple=new Class("Veste", "Chemise");</p>
+
+
+<h1>IV. Quels sont les niveaux de visibilité des propriétés et des méthodes ?</h1>
+
+<p>On peut les voir dans la class parent et enfants ?</p>
+
+
+<h1>V. Qu'est-ce que l'héritage ? </h1>
+
+<p>C'est une class enfant qui herite de la class parent</p>
+
+
+<h1>VI. Quelle est la différence entre une classe et un objet ? </h1>
+
+<p>Une classe prepare l'objet et l'objet affiche les valeurs des propriétés ?</p>
+
+
+<h1>VII. Quelle est la différence entre "public" et "private" ? </h1>
+
+<p>Public est accesible dans et en dehors de la classe, alors que private et accesible que dans la class parent...</p>
+
+
+<h1>VIII. Qu'est-ce qu'une méthode magique ? </h1>
+
+<p></p>
+
+
+<h1>IX. 
+Quelle est l'utilisation de la méthode magique "__toString()" ? </h1>
+
+<p></p>
+
+
+<h1>X. 
+Comment déclare-t-on une constante de classe ? </h1>
+
+<p></p>
+
